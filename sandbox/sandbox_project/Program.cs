7958af085fd7307ee3b1abc35f67b5b3609cf9cf
Jaxon Bake <jaxongbake@gmail.com>
2024-12-07 18:44:23 -0700

    Sandbox

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Dynamic;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        
+
+
+        /*
+        HashSet<int> thisSet = new HashSet<int>() {0, 1, 2, 3};
+        HashSet<int> thisNextSet = new HashSet<int>() {3, 4, 5};
+
+        thisSet.IntersectWith(thisNextSet);
+
+        var interSet = new HashSet<int>();
+        foreach (int i in thisSet) {
+            if (thisNextSet.Contains(i)) {
+                interSet.Add(i);
+            }
+        }
+
+        Console.WriteLine();*/
+
 
         /*
         var thisString = "Hello";
@@ -15,6 +34,7 @@ public class Program
         var revArrToString = new string(thisArray);
 
         Console.WriteLine();*/
+
 
         /*
         List<int> newList = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
